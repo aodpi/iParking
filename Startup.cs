18bcdaacd0fb050eef59e486f127279cc2a5fcd3
aodpi <balan.valeriu@live.com>
2018-01-29 00:57:22 +0200

    Facebook Login

--- Startup.cs
+++ Startup.cs
@@ -12,13 +12,14 @@ namespace iParking
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
-
+        public IHostingEnvironment Environment { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -35,6 +36,26 @@ namespace iParking
             })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            if (Environment.IsProduction())
+            {
+                services.AddAuthentication()
+                    .AddFacebook(opts =>
+                    {
+                        opts.AppId = "410176332753797";
+                        opts.AppSecret = "9f5c3ce91a675680c83fb1f261c6b64d";
+                    });
+
+            }
+            else
+            {
+                services.AddAuthentication()
+                    .AddFacebook(opts =>
+                    {
+                        opts.AppId = "214957042409829";
+                        opts.AppSecret = "fac43c43f17dc5624c37e19517745209";
+                    });
+            }
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
