6afc3c45712bbcb6ae22acca8be11c16540f7730
Valeriu Balan <balan.valeriu@live.com>
2018-04-16 22:35:37 +0300

    feat: Added my reservations

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -23,10 +23,11 @@ namespace iParking.Controllers
         }
 
         // GET: Reservations
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var applicationDbContext = _context.ParkingReservations.Include(p => p.User);
-            return View(await applicationDbContext.ToListAsync());
+            var applicationDbContext = _context.ParkingReservations.Include(p => p.User)
+                .Where(p => p.User.UserName == User.Identity.Name);
+            return View(applicationDbContext);
         }
 
         // GET: Reservations/Details/5
@@ -138,36 +139,6 @@ namespace iParking.Controllers
             }
             ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", parkingReservation.UserId);
             return View(parkingReservation);
-        }
-
-        // GET: Reservations/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var parkingReservation = await _context.ParkingReservations
-                .Include(p => p.User)
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (parkingReservation == null)
-            {
-                return NotFound();
-            }
-
-            return View(parkingReservation);
-        }
-
-        // POST: Reservations/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var parkingReservation = await _context.ParkingReservations.SingleOrDefaultAsync(m => m.Id == id);
-            _context.ParkingReservations.Remove(parkingReservation);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool ParkingReservationExists(int id)
