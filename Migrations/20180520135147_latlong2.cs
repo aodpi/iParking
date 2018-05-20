1d3334af4a44b12128b51602d01110be9d585749
Valeriu Balan <balan.valeriu@live.com>
2018-05-20 16:52:17 +0300

    feat: Added cateogry

---
+++ Migrations/20180520135147_latlong2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace iParking.Migrations
+{
+    public partial class latlong2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CarCategory",
+                table: "ParkingReservations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CarCategory",
+                table: "ParkingReservations",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
