9a27b42a8005f882bf3e16dade6ccd1aa7de4184
Valeriu Balan <balan.valeriu@live.com>
2018-05-20 17:51:04 +0300

    feat: Changed default route

--- Startup.cs
+++ Startup.cs
@@ -83,7 +83,7 @@ namespace iParking
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Reservations}/{action=Create}/{id?}");
             });
         }
     }
