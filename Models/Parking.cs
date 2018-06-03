a17cc059e45d72a0d3d10a6cca4e95e75f9a8d1d
Valeriu Balan <valeriu.balan@outlook.com>
2018-06-03 11:16:07 +0300

    lang fixes

--- Models/Parking.cs
+++ Models/Parking.cs
@@ -1,13 +1,27 @@
-﻿namespace iParking.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace iParking.Models
 {
     public class Parking
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nume parcare")]
         public string ParkingName { get; set; }
+
+        [Display(Name = "Număr parcare")]
         public int ParkingNumber { get; set; }
+
+        [Display(Name = "Locuri parcare")]
         public int ParkingSlots { get; set; }
+
+        [Display(Name = "Preț pe o oră")]
         public decimal PricePerHour { get; set; }
+
+        [Display(Name = "Latitudine")]
         public double Latitude { get; set; }
+        
+        [Display(Name = "Longitudine")]
         public double Longitude { get; set; }
     }
 }
